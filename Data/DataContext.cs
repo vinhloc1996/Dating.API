52a268ae76ac14fc38e7d1531a4f7924fff2e4b9
Nguyễn Bảo Vĩnh Lộc <vinhloc1996@yahoo.com.vn>
2018-01-14 19:08:36 +0700

    Init Commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Dating.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dating.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Value { get; set; }
+    }
+}
